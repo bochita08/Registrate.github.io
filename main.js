3e1254b31c78f6b211a9d33781038a27e47c0acd
bochita08 <127541271+bochita08@users.noreply.github.com>
2024-02-07 11:40:37 -0300

    Update

--- main.js
+++ main.js
@@ -81,6 +81,10 @@ function validarFormulario() {
     return true; // Todas las validaciones son exitosas, se puede enviar el formulario
 }
 
+function enviarFormulario() {
+    getMensaje();
+}
+
 // AJAX - COMUNICACION ASINCRONA CON EL SERVIDOR
 const getMensaje = () => { 
     const http = new XMLHttpRequest();
